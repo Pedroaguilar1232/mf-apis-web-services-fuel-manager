959a8d780f036a9fc5f81f79b10aa699cb376936
Pedroaguilar1232 <pedroaguilar1232@gmail.com>
2024-09-18 13:13:11 -0300

    configuração entity framework

---
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) //ctor tabtab pra chamar o construtor
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+    }
+}
