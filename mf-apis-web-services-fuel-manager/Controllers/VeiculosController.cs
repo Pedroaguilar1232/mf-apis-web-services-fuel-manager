1d9d9eeb6b1cd98b6ec14f0634136fcd404418ed
Pedroaguilar1232 <pedroaguilar1232@gmail.com>
2024-09-18 17:55:58 -0300

    métoco post e get

---
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -0,0 +1,46 @@
+﻿using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context; //injeção de dependência
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+            return Ok(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if(model.AnoFabricaçao <=0 || model.AnoModelo <=0)
+            {
+                return BadRequest(new { message = "Ano de fabricação a ano do modelo são obrigatórios e devem ser maiores do que zero" });
+            }
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetById", new {id = model.Id, model});
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if(model == null) NotFound();
+
+            return Ok(model);
+        }
+    }
+}
